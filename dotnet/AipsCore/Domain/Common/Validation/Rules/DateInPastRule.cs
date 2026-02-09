57dc12d703413d621b97d6f5d335f69aab5fb33d
Veljko Tosic <veljkotosic6@gmail.com>
2026-02-09 19:45:25 +0100

    Basic Date rules

---
+++ dotnet/AipsCore/Domain/Common/Validation/Rules/DateInPastRule.cs
@@ -0,0 +1,22 @@
+using AipsCore.Domain.Abstract.Rule;
+
+namespace AipsCore.Domain.Common.Validation.Rules;
+
+public class DateInPastRule : AbstractRule
+{
+    private readonly DateTime _date;
+    private readonly DateTime _now;
+
+    public DateInPastRule(DateTime date) 
+    {
+        _date = date;
+        _now = DateTime.Now;
+    }
+    
+    protected override string ErrorCode => "date_in_past";
+    protected override string ErrorMessage => "Date must be in the past";
+    public override bool Validate()
+    {
+        return _date < _now;
+    }
+}
