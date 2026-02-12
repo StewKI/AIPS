072d4e3f4646ac6d8f9213c72e64660ad8d10757
Veljko Tosic <veljkotosic6@gmail.com>
2026-02-12 19:50:26 +0100

    Modified user entity for identity

--- dotnet/AipsCore/Infrastructure/Persistence/User/UserRepository.cs
+++ dotnet/AipsCore/Infrastructure/Persistence/User/UserRepository.cs
@@ -1,15 +1,21 @@
+using AipsCore.Domain.Common.Validation;
 using AipsCore.Domain.Models.User.External;
+using AipsCore.Domain.Models.User.Validation;
 using AipsCore.Domain.Models.User.ValueObjects;
 using AipsCore.Infrastructure.Persistence.Abstract;
 using AipsCore.Infrastructure.Persistence.Db;
+using Microsoft.AspNetCore.Identity;
 
 namespace AipsCore.Infrastructure.Persistence.User;
 
 public class UserRepository : AbstractRepository<Domain.Models.User.User, UserId, User>, IUserRepository
 {
-    public UserRepository(AipsDbContext context) 
+    private readonly UserManager<User> _userManager;
+    
+    public UserRepository(AipsDbContext context, UserManager<User> userManager) 
         : base(context)
     {
+        _userManager = userManager;
     }
 
     protected override Domain.Models.User.User MapToModel(User entity)
@@ -17,7 +23,7 @@ public class UserRepository : AbstractRepository<Domain.Models.User.User, UserId
         return Domain.Models.User.User.Create(
             entity.Id.ToString(),
             entity.Email,
-            entity.Username,
+            entity.UserName,
             entity.CreatedAt,
             entity.DeletedAt
         );
@@ -29,7 +35,9 @@ public class UserRepository : AbstractRepository<Domain.Models.User.User, UserId
         {
             Id = new Guid(model.Id.IdValue),
             Email = model.Email.EmailValue,
-            Username = model.Username.UsernameValue,
+            NormalizedEmail = model.Email.EmailValue.ToUpperInvariant(),
+            UserName = model.Username.UsernameValue,
+            NormalizedUserName = model.Username.UsernameValue.ToUpperInvariant(),
             CreatedAt = model.CreatedAt.CreatedAtValue,
             DeletedAt = model.DeletedAt.DeletedAtValue
         };
@@ -38,8 +46,47 @@ public class UserRepository : AbstractRepository<Domain.Models.User.User, UserId
     protected override void UpdateEntity(User entity, Domain.Models.User.User model)
     {
         entity.Email = model.Email.EmailValue;
-        entity.Username = model.Username.UsernameValue;
+        entity.NormalizedEmail = model.Email.EmailValue.ToUpperInvariant();
+        entity.UserName = model.Username.UsernameValue;
+        entity.NormalizedUserName = model.Username.UsernameValue.ToUpperInvariant();
         entity.CreatedAt = model.CreatedAt.CreatedAtValue;
         entity.DeletedAt = model.DeletedAt.DeletedAtValue;
+    }
+
+    public async Task SignUpWithPasswordAsync(Domain.Models.User.User user, string password, CancellationToken cancellationToken = default)
+    {
+        var entity = MapToEntity(user);
+        
+        var result = await _userManager.CreateAsync(entity, password);
+        
+        if (!result.Succeeded)
+        {
+            var errors = string.Join(", ", result.Errors.Select(e => e.Description));
+            throw new Exception($"User registration failed: {errors}");
+        }
+        
+        //Realno nebitno, ali postoji infrastruktura ako treba da se koristi kasnije
+        //await _userManager.AddToRoleAsync(entity, "User");
+    }
+
+    public async Task<LoginResult> LoginWithEmailAndPasswordAsync(string email, string password, CancellationToken cancellationToken = default)
+    {
+        var entity = await _userManager.FindByEmailAsync(email);
+
+        if (entity is null)
+        {
+            throw new ValidationException(UserErrors.LoginErrorUserNotFoundByEmail(email));
+        }
+
+        var isPasswordValid = await _userManager.CheckPasswordAsync(entity, password);
+
+        if (!isPasswordValid)
+        {
+            throw new ValidationException(UserErrors.LoginErrorIncorrectPassword());
+        }
+
+        var roles = await _userManager.GetRolesAsync(entity);
+        
+        return new LoginResult(MapToModel(entity), roles);
     }
 }
