cbc27238ad0c1f4c3b4d8f39bf7c0316a61b410e
Veljko Tosic <veljkotosic6@gmail.com>
2026-02-14 18:49:11 +0100

    Updated DB to use refresh tokens, added migration

--- dotnet/AipsCore/Infrastructure/Persistence/Db/AipsDbContext.cs
+++ dotnet/AipsCore/Infrastructure/Persistence/Db/AipsDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AipsCore.Infrastructure.Persistence.Authentication.RefreshToken;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,8 @@ namespace AipsCore.Infrastructure.Persistence.Db;
 
 public class AipsDbContext : IdentityDbContext<User.User, IdentityRole<Guid>, Guid>
 {
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    
     public DbSet<Whiteboard.Whiteboard> Whiteboards { get; set; }
     public DbSet<Shape.Shape> Shapes { get; set; }
     public DbSet<WhiteboardMembership.WhiteboardMembership> WhiteboardMemberships { get; set; }
