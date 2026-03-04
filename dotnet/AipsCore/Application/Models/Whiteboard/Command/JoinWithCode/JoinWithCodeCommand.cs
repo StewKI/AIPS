88442f22ddd5246a135697b94b8e6ab8c32db574
Veljko Tosic <veljkotosic6@gmail.com>
2026-03-04 23:10:48 +0100

    Join with code endpoint and command

---
+++ dotnet/AipsCore/Application/Models/Whiteboard/Command/JoinWithCode/JoinWithCodeCommand.cs
@@ -0,0 +1,5 @@
+using AipsCore.Application.Abstract.Command;
+
+namespace AipsCore.Application.Models.Whiteboard.Command.JoinWithCode;
+
+public record JoinWithCodeCommand(string Code): ICommand<JoinWithCodeDto>;
