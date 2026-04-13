2ef5aca6e62f97f75337beed9499415c7c6dc531
Veljko Tosic <veljkotosic6@gmail.com>
2026-04-13 16:52:28 +0200

    cleanup

--- dotnet/AipsIntegrationTests/Abstract/IntegrationTestBase.cs
+++ dotnet/AipsIntegrationTests/Abstract/IntegrationTestBase.cs
@@ -3,7 +3,6 @@ using AipsCore.Infrastructure.Persistence.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Testcontainers.PostgreSql;
 
 namespace AipsIntegrationTests.Abstract;
--- dotnet/AipsTestsUtility/AssertUtility.cs
+++ dotnet/AipsTestsUtility/AssertUtility.cs
@@ -24,4 +24,20 @@ public static class AssertUtility
                 $"Received codes: [{receivedCodesString}]");
         });
     }
+    
+    public static void AssertHasBrokenRule<TRule>(ValidationException exception)
+        where TRule : IRuleMetadata
+    {
+        var expectedCode = TRule.ErrorCodeString;
+
+        var receivedCodes = exception.ValidationErrors.Select(e => e.Code).ToList();
+        var receivedCodesString = string.Join(", ", receivedCodes.Select(c => $"'{c}'"));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(receivedCodes, Contains.Item(expectedCode), 
+                $"Expected validation error code '{expectedCode}' was not found. " +
+                $"Received codes: [{receivedCodesString}]");
+        });
+    }
 }
